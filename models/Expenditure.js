5276d5d96b4e22975b9d65e7913331ce9e72933b
Weston Peters <westonrpr@yahoo.com>
2020-11-03 13:24:35 -0800

    more updates

--- models/Expenditure.js
+++ models/Expenditure.js
@@ -1,17 +1,25 @@
 const mongoose = require('mongoose');
+const {DateTime} = require('luxon');
 
 const Schema = mongoose.Schema;
 
 
 const expenditureSchema = new Schema(
   {
-    amount: {type: Number, maxlength: 50},
-    necessity: {type: Boolean},
-    date: {type: Date, default: Date.now},
-    category: {type: String, maxlength: 100},
+    title: {type: String, maxlength: 30, required: true},
+    amount: {type: Number, maxlength: 50, required: true},
+    necessity: {type: Boolean, required: true},
+    category: {type: String, maxlength: 100, required: true},
+    timeStamp: {type: Date, default: Date.now},
     userId: {type: String} 
   }
 );
+
+
+expenditureSchema.virtual('date')
+.get(function() {
+  return DateTime.fromJSDate(this.timeStamp).toLocaleString(DateTime.DATE_MED);
+})
 
  
 module.exports = mongoose.model('Expenditure', expenditureSchema);
