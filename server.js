a04971f92c3b5682fe8a7a1b54de476db183293e
Weston Peters <westonrpr@yahoo.com>
2021-10-14 10:06:04 -0500

    change to allow rendering of ejs files
    Since this application is no longer connected to any database, files needed to be reconfigured to allow rendering of EJS files. This application has no utility besides the ejs files.

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded());
+
+app.use('/', require('./routes/indexRoute'));
+app.use('/', require('./routes/loginRoute'));
+app.use('/', require('./routes/logoutRoute'));
+app.use('/', require('./routes/profileSetUpRoute'));
+app.use('/', require('./routes/signupRoute'));
+app.use('/', require('./routes/userProfileRoute'));
+
+app.get('/', (req, res) => {
+  res.send('hello from server');
+});
+const PORT = process.env.port || 3000;
+app.listen(PORT, console.log(`listening on port: ${PORT}`));
