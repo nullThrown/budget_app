267e40e1acbb0e57d4e72a905db614615eff1342
Weston Peters <westonrpr@yahoo.com>
2021-10-15 12:20:24 -0500

    add main container

--- client/src/components/profile/expenditures/expenditures.js
+++ client/src/components/profile/expenditures/expenditures.js
@@ -1,11 +1,15 @@
 import '../expenditures/expenditures.css';
 import { Header } from './heading';
 import { Nav } from './nav';
+import { Table } from './table';
 export const Expenditures = () => {
   return (
     <section className='expenditures-box'>
       <Header />
-      <Nav />
+      <main className='main'>
+        <Nav />
+        <Table />
+      </main>
     </section>
   );
 };
