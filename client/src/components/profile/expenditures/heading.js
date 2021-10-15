f50223fc34b206467db56aa5596dc83e8d957c68
Weston Peters <westonrpr@yahoo.com>
2021-10-15 12:21:31 -0500

    rename css classes to appropriate component

--- client/src/components/profile/expenditures/heading.js
+++ client/src/components/profile/expenditures/heading.js
@@ -1,13 +1,13 @@
 export const Header = () => {
   return (
     <header>
-      <h2 className='expenditures-title'>Expenditures</h2>
-      <div className='expenditures-desc'>
-        <h3 className='expenditures-category'>
+      <h2 className='header-title'>Expenditures</h2>
+      <div className='header-desc'>
+        <h3>
           <small className='text-sm-i'>category</small> Dining
         </h3>
         <h4>budget: $300</h4>
-        <p className='category-spent'>
+        <p>
           {/* add a percentage of budget spent in line form */}
           {/* spent amount should be green or red and much larger than other text */}
           spent: $135.32 <small className='text-sm-i'>of</small> $300
