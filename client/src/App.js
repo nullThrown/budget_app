6de74ff416edc686dd2b0dffcd0d4fc148834ff6
Weston Peters <westonrpr@yahoo.com>
2021-10-17 11:55:09 -0500

    import profile components for UI testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
 import { Expenditures } from './components/profile/expenditures/expenditures';
-
+import { Budget } from './components/profile/budget/budget';
+import { Header } from './components/profile/header/header';
 function App() {
-  return <Expenditures />;
+  return (
+    <main className='main-container'>
+      <Header />
+    </main>
+  );
 }
 
 export default App;
