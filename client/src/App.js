e01f22d30f7552976834f8c6abdbd382c22221bd
Weston Peters <westonrpr@yahoo.com>
2021-10-20 13:52:25 -0500

    add newly created components for testing

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import { Expenditures } from './components/profile/expenditures/expenditures';
 import { Budget } from './components/profile/budget/budget';
 import { Header } from './components/profile/header/header';
+import { Login } from './components/form/login/login';
+import { Signup } from './components/form/signup/signup';
 function App() {
   return (
     <main className='main-container'>
-      <Header />
+      <Signup />
     </main>
   );
 }
