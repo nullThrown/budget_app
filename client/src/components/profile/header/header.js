c421c8bc05ee2c3f5d64389e31207dfc91dc636c
Weston Peters <westonrpr@yahoo.com>
2021-10-20 14:01:57 -0500

    change icons from fontAwesome to react-icons

--- client/src/components/profile/header/header.js
+++ client/src/components/profile/header/header.js
@@ -1,13 +1,18 @@
+import { useState } from 'react';
 import './header.css';
+import { IconContext } from 'react-icons';
+import { BiUserCircle } from 'react-icons/bi';
+import { IoNotificationsOutline } from 'react-icons/io5';
 export const Header = () => {
+  const [isNotification, setIsNotification] = useState(true);
   return (
     <header className='header'>
-      <h2 className='heading-2 title'>Header</h2>
+      <h2 className='heading-2 title'>Dashboard</h2>
       <div className='user-nav'>
-        <i class='far fa-calendar-alt'></i>
-        <i class='far fa-chart-bar'></i>
-        <i class='far fa-bell'></i>
-        <i class='far fa-user-circle user'></i>
+        <span className={isNotification ? 'notification-active' : ''}>
+          <IoNotificationsOutline size={'1.8em'} className={'icon-user'} />
+        </span>
+        <BiUserCircle size={'2.4em'} />
       </div>
     </header>
   );
