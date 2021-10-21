2aac6162f204c137f59abde8de429a7c70b7a0e6
Weston Peters <westonrpr@yahoo.com>
2021-10-21 12:48:21 -0500

    create signup password validation

---
+++ client/src/util/validation/validatePassword.js
@@ -0,0 +1,33 @@
+// export const checkPasswordLength = (elName, elValue) => {
+//   if (elName === 'password') {
+//     if (elValue.length >= 8) return setIsPwordLong(true);
+
+//     return setIsPwordLong(false);
+//   }
+// };
+
+// //check password has one number
+// export const checkPasswordNum = (elName, elValue) => {
+//   if (elName === 'password') {
+//     if (elValue.match('.*[0-9].*')) return setIsPwordHasNum(true);
+
+//     return setIsPwordHasNum(false);
+//   }
+// };
+// // check password has one special character
+// export const checkPasswordSym = (elName, elValue) => {
+//   if (elName === 'password') {
+//     if (elValue.match('(?=.*?[#?!@$%^&*-])')) return setIsPwordHasSym(true);
+
+//     return setIsPwordHasSym(false);
+//   }
+// };
+
+// // check password has one uppercases character
+// export const checkPasswordUpper = (elName, elValue) => {
+//   if (elName === 'password') {
+//     if (elValue.match('(?=.*?[A-Z])')) return setIsPwordHasUpper(true);
+
+//     return setIsPwordHasUpper(false);
+//   }
+// };
