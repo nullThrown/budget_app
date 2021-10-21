bee423fd937c1dcd0fc7da183cac88bdb04e0122
Weston Peters <westonrpr@yahoo.com>
2021-10-21 12:47:31 -0500

    create alert component

---
+++ client/src/components/alert/alert.js
@@ -0,0 +1,36 @@
+import { GiCheckMark } from 'react-icons/gi';
+import { MdDangerous } from 'react-icons/md';
+import './alert.css';
+
+export const AlertBg = (props) => {
+  const { msg, isSuccess } = props;
+  console.log(isSuccess);
+  return (
+    <figure>
+      <figcaption
+        className={
+          'flex align-center alert ' +
+          (isSuccess
+            ? 'alert--success alert--success-bg'
+            : 'alert--failure alert--failure-bg')
+        }>
+        {isSuccess ? <GiCheckMark /> : <MdDangerous />} {msg}
+      </figcaption>
+    </figure>
+  );
+};
+export const Alert = (props) => {
+  const { msg, isSuccess } = props;
+  console.log(isSuccess);
+  return (
+    <figure>
+      <figcaption
+        className={
+          'flex align-center alert ' +
+          (isSuccess ? 'alert--success ' : 'alert--failure')
+        }>
+        {isSuccess ? <GiCheckMark /> : <MdDangerous />} {msg}
+      </figcaption>
+    </figure>
+  );
+};
