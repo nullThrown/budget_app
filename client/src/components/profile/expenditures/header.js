6e49d5bb478a3b3b07d625d4bce08171afa7399d
Weston Peters <westonrpr@yahoo.com>
2021-10-26 17:52:14 -0500

    rework header description section

--- client/src/components/profile/expenditures/header.js
+++ client/src/components/profile/expenditures/header.js
@@ -1,3 +1,4 @@
+import { IoIosArrowDown } from 'react-icons/io';
 export const Header = () => {
   return (
     <header>
@@ -5,16 +6,22 @@ export const Header = () => {
         <h2 className='heading-3'>Expenditures</h2>
         <button className='btn'>Add Expenditure</button>
       </div>
-      <div className='header-desc'>
-        <h3>
-          <small className='text-sm-i'>category</small> Dining
-        </h3>
-        <h4>budget: $300</h4>
-        <p className='header-spent'>
-          {/* add a percentage of budget spent in line form */}
-          {/* spent amount should be green or red and much larger than other text */}
-          spent: $135.32 <small className='text-sm-i'>of</small> $300
-        </p>
+      <div className='desc-box'>
+        <div className='header-desc'>
+          <div className='desc-category'>
+            <button className='btn category-btn'>
+              Category <IoIosArrowDown className={'category-btn-icon'} />
+            </button>
+            <h3 className='heading-5 current-category'>Dining</h3>
+          </div>
+          <div className='header-spent'>
+            <p>
+              spent: <span className='text-bg-red'>$135.32</span>
+            </p>
+            <small className='text-sm-i'> of </small>
+            <p className='text-bg-green'> $300</p>
+          </div>
+        </div>
       </div>
     </header>
   );
