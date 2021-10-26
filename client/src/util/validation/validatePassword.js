0b030c0f77e92f388a58b0845a22dd92e91e7eff
Weston Peters <westonrpr@yahoo.com>
2021-10-26 16:06:11 -0500

    remove name check for each validator

--- client/src/util/validation/validatePassword.js
+++ client/src/util/validation/validatePassword.js
@@ -1,35 +1,27 @@
 // checks password strength upon sign up
 
-export const checkPasswordLength = (elName, elValue, cb) => {
-  if (elName === 'password') {
-    if (elValue.length >= 8) return cb(true);
+export const checkPasswordLength = (password, cb) => {
+  if (password.length >= 8) return cb(true);
 
-    return cb(false);
-  }
+  return cb(false);
 };
 
 // check password has one number
-export const checkPasswordNum = (elName, elValue, cb) => {
-  if (elName === 'password') {
-    if (elValue.match('.*[0-9].*')) return cb(true);
+export const checkPasswordNum = (password, cb) => {
+  if (password.match('.*[0-9].*')) return cb(true);
 
-    return cb(false);
-  }
+  return cb(false);
 };
 // check password has one special character
-export const checkPasswordSym = (elName, elValue, cb) => {
-  if (elName === 'password') {
-    if (elValue.match('(?=.*?[#?!@$%^&*-])')) return cb(true);
+export const checkPasswordSym = (password, cb) => {
+  if (password.match('(?=.*?[#?!@$%^&*-])')) return cb(true);
 
-    return cb(false);
-  }
+  return cb(false);
 };
 
 // check password has one uppercases character
-export const checkPasswordUpper = (elName, elValue, cb) => {
-  if (elName === 'password') {
-    if (elValue.match('(?=.*?[A-Z])')) return cb(true);
+export const checkPasswordUpper = (password, cb) => {
+  if (password.match('(?=.*?[A-Z])')) return cb(true);
 
-    return cb(false);
-  }
+  return cb(false);
 };
