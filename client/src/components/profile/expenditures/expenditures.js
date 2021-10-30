ffdd5fe53c82afb826609d13d306b69f48a0c75b
Weston Peters <westonrpr@yahoo.com>
2021-10-30 13:52:18 -0500

    remove unused component: nav

--- client/src/components/profile/expenditures/expenditures.js
+++ client/src/components/profile/expenditures/expenditures.js
@@ -1,13 +1,12 @@
+import React, { useState } from 'react';
 import '../expenditures/expenditures.css';
 import { Header } from './header';
-import { Nav } from './nav';
 import { Table } from './table';
 export const Expenditures = () => {
   return (
     <section className='expenditures-box'>
       <Header />
       <main className='main'>
-        {/* <Nav /> */}
         <Table />
       </main>
     </section>
