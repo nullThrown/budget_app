ae3d4a0652bb9ba447570c79e0031f973d0ecc6b
Weston Peters <westonrpr@yahoo.com>
2021-11-11 17:44:25 -0600

    no change

--- server.js
+++ server.js
@@ -16,5 +16,5 @@ app.use('/', require('./routes/userProfileRoute'));
 app.get('/', (req, res) => {
   res.send('hello from server');
 });
-const PORT = process.env.port || 3000;
+const PORT = process.env.port || 4000;
 app.listen(PORT, console.log(`listening on port: ${PORT}`));
