4b8c62ec8cc710e5c50df90c099d2339daff5b4f
Weston Peters <westonrpr@yahoo.com>
2021-11-13 15:11:23 -0600

    change component for testing purposes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import { Expenditures } from './components/profile/expenditures/expenditures';
 import { Budget } from './components/profile/budget/budget';
+import { MonthlySpecifics } from './components/profile/monthlySpecifics/monthlySpecifics';
 import { Header } from './components/profile/header/header';
 import { Login } from './components/form/login/login';
 import { Signup } from './components/form/signup/signup';
 function App() {
   return (
     <main className='main-container'>
-      <Expenditures />
+      <MonthlySpecifics />
     </main>
   );
 }
