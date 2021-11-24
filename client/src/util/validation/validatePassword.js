fbb4b77e25222e79905aafea3c6d71c16c2193ee
Weston Peters <westonrpr@yahoo.com>
2021-11-23 18:02:13 -0600

    change parameters from 'password' to 'string'

--- client/src/util/validation/validatePassword.js
+++ client/src/util/validation/validatePassword.js
@@ -1,27 +1,27 @@
-// checks password strength upon sign up
+// checks string strength upon sign up
 
-export const checkPasswordLength = (password, cb) => {
-  if (password.length >= 8) return cb(true);
-
-  return cb(false);
-};
-
-// check password has one number
-export const checkPasswordNum = (password, cb) => {
-  if (password.match('.*[0-9].*')) return cb(true);
-
-  return cb(false);
-};
-// check password has one special character
-export const checkPasswordSym = (password, cb) => {
-  if (password.match('(?=.*?[#?!@$%^&*-])')) return cb(true);
+export const checkPasswordLength = (string, cb) => {
+  if (string.length >= 8) return cb(true);
 
   return cb(false);
 };
 
-// check password has one uppercases character
-export const checkPasswordUpper = (password, cb) => {
-  if (password.match('(?=.*?[A-Z])')) return cb(true);
+// check string has one number
+export const checkPasswordNum = (string, cb) => {
+  if (string.match('.*[0-9].*')) return cb(true);
+
+  return cb(false);
+};
+// check string has one special character
+export const checkPasswordSym = (string, cb) => {
+  if (string.match('(?=.*?[#?!@$%^&*-])')) return cb(true);
+
+  return cb(false);
+};
+
+// check string has one uppercases character
+export const checkPasswordUpper = (string, cb) => {
+  if (string.match('(?=.*?[A-Z])')) return cb(true);
 
   return cb(false);
 };
