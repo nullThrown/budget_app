c638dddc3c5694ad53abf7b5c78b38e9e725ab69
github_username <you@domain.com>
2021-12-05 19:53:21 -0600

    create intro component

---
+++ client/src/components/landing/intro.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+ const Intro = () => {
+    return (
+        <section className='land__intro'>
+            <h2 className='heading-4 text-center'>Track Your Budget</h2>
+            <div className="land__content-box">
+                <p className='land__intro-desc'>Coin Drop is a daily budgeter that tracks every dollar in and out of your person. Any time money is spent or earned, add it to your expenditures. Money spent on the grocery store, housing, retirement accounts -- all of it is tracked with Dimedrop.</p>
+                <div className='fake-img-4'></div>
+            </div>
+
+        </section>            
+    )
+}
+
+export default Intro;
