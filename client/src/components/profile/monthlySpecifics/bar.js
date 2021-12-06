f40f30d52c2e0a76bfab6f1ad22a351833c82e87
github_username <you@domain.com>
2021-12-05 19:48:17 -0600

    remove local state for props

--- client/src/components/profile/monthlySpecifics/bar.js
+++ client/src/components/profile/monthlySpecifics/bar.js
@@ -1,72 +1,8 @@
 import { Bar } from 'react-chartjs-2';
 import './monthlySpecifics.css';
-const categories = [
-  {
-    title: 'vehicle',
-    budget: 500,
-    spent: 255,
-  },
-  {
-    title: 'misc',
-    budget: 400,
-    spent: 123.34,
-  },
-  {
-    title: 'Dining',
-    budget: 200,
-    spent: 65.33,
-  },
-  {
-    title: 'utlities',
-    budget: 400,
-    spent: 356,
-  },
-  {
-    title: 'vehicle',
-    budget: 500,
-    spent: 255,
-  },
-  {
-    title: 'misc',
-    budget: 400,
-    spent: 123.34,
-  },
-  {
-    title: 'Dining',
-    budget: 200,
-    spent: 65.33,
-  },
-];
-const data = {
-  labels: categories.map((cat) => cat.title),
-  datasets: [
-    {
-      label: 'Money Spent',
-      data: categories.map((cat) => cat.spent),
-      backgroundColor: 'rgb(210, 168, 162)',
-      stack: 'Stack 0',
-    },
-    {
-      label: 'Budget',
-      data: categories.map((cat) => cat.budget),
-      backgroundColor: 'rgba(203, 246, 203, 0.9)',
-      stack: 'Stack 0',
-    },
-  ],
-};
-const options = {
-  chart: {
-    dataset: {
-      data: {},
-    },
-    scale: {
-      tick: {},
-      pointlabel: {},
-    },
-    tooltip: {},
-  },
-};
-const BarGraph = () => {
+
+
+const BarGraph = ({data}) => {
   return (
     <section className='card monthly-specifics'>
       <h2 className='heading-4 text-center'>Monthly Specifics</h2>
