743e32e8100bc9c04be9fe4ae71c63f5cec3101e
github_username <you@domain.com>
2021-12-05 19:50:39 -0600

    imported all page components

--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import Header from '../components/landing/header'
+import Description from '../components/landing/description'
+import Intro from '../components/landing/intro'
+import Footer from '../components/landing/footer'
 
 export const Landing = () => {
-  return <h1>Landing</h1>;
+  return (
+    <>
+    <Header/>
+    <Intro/>
+    <Description/>
+    <Footer/>
+    </>
+  )
 };
 export default Landing;
