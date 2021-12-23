7ad5edca77d3d855fd46cb40824cd2013eecd21a
Weston Peters <westonrpr@yahoo.com>
2021-12-22 22:06:57 -0600

    add hamburger menu icon

--- client/src/components/nav/nav.js
+++ client/src/components/nav/nav.js
@@ -1,23 +1,48 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import './nav.css'
-import {AiFillHome,AiFillCalendar} from 'react-icons/ai'
-import {GiTreeGrowth, GiTwoCoins} from 'react-icons/gi'
-import {BiHelpCircle} from 'react-icons/bi'
- const Nav = () => {
-    return (
-        <aside className='sidebar'>
-            <h2 className='heading-4 flex sidebar__heading'><GiTwoCoins/> CoinDrop</h2>
-            <nav className='sidebar__nav'>
-                <ul className='flex-col'>
-                    <li className='nav__list-item'><AiFillHome className='nav__icons'/><Link to='../home' className='nav__link-item'>Home</Link></li>
-                    <li className='nav__list-item'><AiFillCalendar className='nav__icons'/><Link to='../year' className='nav__link-item'>Year</Link></li>
-                    <li className='nav__list-item'><GiTreeGrowth className='nav__icons'/><Link to='/' className='nav__link-item'>Retirement</Link></li>
-                    <li className='nav__list-item'><BiHelpCircle className='nav__icons'/><Link to='/' className='nav__link-item'>Help</Link></li>
-                </ul>
-            </nav>
-        </aside>
-    )
-}
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './nav.css';
+import { AiFillHome, AiFillCalendar } from 'react-icons/ai';
+import { GiTreeGrowth, GiTwoCoins, GiHamburgerMenu } from 'react-icons/gi';
+import { BiHelpCircle } from 'react-icons/bi';
+const Nav = () => {
+  return (
+    <aside className='sidebar'>
+      <div className='sidebar__title-box'>
+        <GiHamburgerMenu className='sidebar__menu-icon' />
+        <h2 className='heading-5 sidebar__header'>
+          <GiTwoCoins /> CoinDrop
+        </h2>
+      </div>
+      <nav className='sidebar__nav'>
+        <ol className='flex-col'>
+          <li className='nav__list-item'>
+            <AiFillHome className='nav__icons' />
+            <Link to='../home' className='nav__link-item'>
+              Home
+            </Link>
+          </li>
+          <li className='nav__list-item'>
+            <AiFillCalendar className='nav__icons' />
+            <Link to='../year' className='nav__link-item'>
+              Year
+            </Link>
+          </li>
+          <li className='nav__list-item'>
+            <GiTreeGrowth className='nav__icons' />
+            <Link to='/' className='nav__link-item'>
+              Retirement
+            </Link>
+          </li>
+          <li className='nav__list-item'>
+            <BiHelpCircle className='nav__icons' />
+            <Link to='/' className='nav__link-item'>
+              Help
+            </Link>
+          </li>
+        </ol>
+      </nav>
+    </aside>
+  );
+};
 
 export default Nav;
