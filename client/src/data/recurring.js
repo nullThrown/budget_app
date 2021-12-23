d08d36a41cbf040846c7bc4bf31d8be6cd43f504
Weston Peters <westonrpr@yahoo.com>
2021-12-22 23:39:43 -0600

    formatting

--- client/src/data/recurring.js
+++ client/src/data/recurring.js
@@ -1,62 +1,54 @@
-const recurring = [
-    {
-        category: 'housing', 
-        budget:  2000,
-        exp: [
-            {
-                name: 'rent',
-                amount: 1200
-            },
-            {
-                name: 'rent',
-                amount: 1200
-            },
-            {
-                name: 'rent',
-                amount: 1200
-            },
-            {
-                name: 'rent',
-                amount: 1200
-            }
-        ]   
-    },
-    {
-        category: 'vehicle', 
-        budget: 1000,
-        exp: [
-            {
-                name: 'insurance',
-                amount: 200
-            },
-            {
-                name: 'car payment',
-                amount: 256
-            }
-        ]   
-    },
-    {
-        category: 'utility', 
-        budget: 2000,
-        exp: [
-            {
-                name: 'garbage',
-                amount: 100
-            },
-            {
-                name: 'electricity',
-                amount: 250
-            },
-            {
-                name: 'gardening',
-                amount: 150
-            },
-            {
-                name: 'rent',
-                amount: 1200
-            }
-        ]   
-    },
-]
+const recurringData = [
+  {
+    title: 'housing',
+    budget: 2000,
+    exp: [
+      {
+        name: 'rent',
+        amount: 1200,
+      },
+      {
+        name: 'fire insurance',
+        amount: 50,
+      },
+      {
+        name: 'supplies',
+        amount: 100,
+      },
+    ],
+  },
+  {
+    title: 'vehicle',
+    budget: 1000,
+    exp: [
+      {
+        name: 'insurance',
+        amount: 200,
+      },
+      {
+        name: 'car payment',
+        amount: 256,
+      },
+    ],
+  },
+  {
+    title: 'utility',
+    budget: 2000,
+    exp: [
+      {
+        name: 'garbage',
+        amount: 100,
+      },
+      {
+        name: 'electricity',
+        amount: 250,
+      },
+      {
+        name: 'gardening',
+        amount: 150,
+      },
+    ],
+  },
+];
 
-export default recurring;
+export default recurringData;
