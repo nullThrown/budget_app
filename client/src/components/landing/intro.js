946b09a273737818bfb1b5643517cbc3998c27b8
Weston Peters <westonrpr@yahoo.com>
2021-12-22 22:04:02 -0600

    add hook text and lorem ipsum placehold for main text content

--- client/src/components/landing/intro.js
+++ client/src/components/landing/intro.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React from 'react';
 
- const Intro = () => {
-    return (
-        <section className='land__intro'>
-            <h2 className='heading-4 text-center'>Track Your Budget</h2>
-            <div className="land__content-box">
-                <p className='land__intro-desc'>Coin Drop is a daily budgeter that tracks every dollar in and out of your person. Any time money is spent or earned, add it to your expenditures. Money spent on the grocery store, housing, retirement accounts -- all of it is tracked with Dimedrop.</p>
-                <div className='fake-img-4'></div>
-            </div>
-
-        </section>            
-    )
-}
+const Intro = () => {
+  return (
+    <section className='land__intro'>
+      <p className='text-center'>How does coin drop work?</p>
+      <div className='intro__content-box'>
+        <figure className='intro__text-box'>
+          <h2 className='heading-4 text-center'>Track Your Budget</h2>
+          <p className='intro__text'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo,
+            quidem nisi, eius minima sed esse ducimus illum animi iste pariatur
+            labore? Culpa sapiente, modi accusamus molestiae beatae quae
+            asperiores reiciendis iste officia mollitia atque facere distinctio
+            amet. Cumque, excepturi unde! Ullam voluptates enim consequuntur
+            harum obcaecati blanditiis provident asperiores est cupiditate
+            corrupti! Laboriosam nam distinctio quam, doloremque, facere ducimus
+            delectus nesciunt reprehenderit consequuntur nobis, dolorem quas
+            laudantium. Magni optio ad unde voluptatibus magnam error quaerat
+            harum ipsa architecto beatae! Culpa, corrupti! Accusantium nihil
+            reiciendis repellat nesciunt sint eligendi quo sed?
+          </p>
+        </figure>
+        <div className='fake-img-4'></div>
+      </div>
+    </section>
+  );
+};
 
 export default Intro;
