0e2e867cd83fb0f4669b7915ea11d47381f64195
Weston Peters <westonrpr@yahoo.com>
2021-12-25 11:32:26 -0600

    remove whitespace

--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -3,7 +3,6 @@ import Header from '../components/landing/header';
 import Description from '../components/landing/description';
 import Intro from '../components/landing/intro';
 import Footer from '../components/landing/footer';
-
 export const Landing = () => {
   return (
     <>
