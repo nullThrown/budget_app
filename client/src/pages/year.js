bd25d6c9425c58f843c89a2c6f34e0387089f4fc
Weston Peters <westonrpr@yahoo.com>
2021-12-25 11:33:55 -0600

    imported components name change

--- client/src/pages/year.js
+++ client/src/pages/year.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import Budget from '../components/profile/budget/budget';
+import BudgetGraph from '../components/profile/budget/budgetGraph';
 import Expenditures from '../components/profile/expenditures/expenditures';
 import Header from '../components/profile/header/header';
-import Bar from '../components/profile/monthlySpecifics/bar';
+import Overview from '../components/profile/overview/overview';
 import Banner from '../components/profile/banner/banner';
-import {budgetData, categories, barData, expData} from '../data/year'
+import { budgetData, categories, barData, expData } from '../data/year';
 
 const Home = () => {
   return (
     <main className='main-container'>
-     <Header />
+      <Header />
       <Banner />
-      <Budget data={budgetData} />
-      <Bar data={barData} categories={categories}/>
-      <Expenditures data={expData}/>
+      <BudgetGraph data={budgetData} />
+      <Overview data={barData} categories={categories} />
+      <Expenditures data={expData} />
     </main>
   );
 };
