7342748b2050aac90917b26bcb62a1765b0c7148
Weston Peters <westonrpr@yahoo.com>
2022-01-04 11:40:43 -0600

    add state selectors from redux store

--- client/src/components/profile/overview/overview.js
+++ client/src/components/profile/overview/overview.js
@@ -1,7 +1,18 @@
 import './overview.css';
 import { Doughnut } from 'react-chartjs-2';
+import { useSelector } from 'react-redux';
+import { doughnutData } from '../../../data/currentMonth';
 
 const Budget = ({ doughnut, budget }) => {
+  const selectProfileData = (state) => {
+    const recurringPayments = state.profile.recurringPayments;
+    return recurringPayments.reduce((acc, obj) => {
+      return acc + obj.total;
+    }, 0);
+  };
+  const takeHomeAmount = useSelector((state) => state.profile.monthlyTakeHome);
+  const profileData = useSelector(selectProfileData);
+  const DoughnutData = [takeHomeAmount, profileData, 100];
   return (
     <section className='card budget'>
       <h3 className='heading-4 text-center'>Overview</h3>
@@ -9,16 +20,16 @@ const Budget = ({ doughnut, budget }) => {
         <BudgetItem
           title={'Take-home pay'}
           amountClass={'item-amount--green'}
-          amount={budget.takeHomePay}
+          amount={takeHomeAmount}
         />
-        <Doughnut data={doughnut} className='pie-chart' />
+        <Doughnut data={doughnut(DoughnutData)} className='pie-chart' />
       </div>
     </section>
   );
 };
 
 //choose better name than item
-// keep this in case all data is to be displayed in plain text
+// keep this in case all amounts are to be presented explicitly
 const BudgetItem = (props) => {
   const { title, amountClass, amount } = props;
   return (
