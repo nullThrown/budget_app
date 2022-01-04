dc97947d17ed58c7c88126d3f938028741f7e27d
Weston Peters <westonrpr@yahoo.com>
2022-01-04 11:37:26 -0600

    add redux state select and dispatch
    *selected expenditures and profile.recurringPayments *dispatched a getProfile to grab data from db, *this component is a little convoluted at the moment. It will take a little bit of time to organize redux state and our db. In the meantime, things will stay a little messy.

--- client/src/components/profile/budget/budgetGraph.js
+++ client/src/components/profile/budget/budgetGraph.js
@@ -1,11 +1,26 @@
+import { useEffect, useState } from 'react';
 import { Bar } from 'react-chartjs-2';
 import './budgetGraph.css';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { getProfile } from '../../../features/profile/profileSlice';
+const BudgetGraph = ({ data, categories }) => {
+  const dispatch = useDispatch();
+  const selectPayments = (state) => {
+    return state.profile.recurringPayments.map(
+      ({ total, budget, category }) => ({ total, budget, category })
+    );
+  };
+  dispatch(getProfile());
+  const payments = useSelector(selectPayments);
+  console.log(payments);
+  const expenditures = useSelector((state) => state.expenditures);
+  const [budgetData, setBudgetData] = useState([]);
 
-const BudgetGraph = ({ data }) => {
   return (
     <section className='card monthly-specifics'>
       <h2 className='heading-4 text-center'>Budget</h2>
-      <Bar data={data} className='monthly-graph' />
+      <Bar data={data(categories)} className='monthly-graph' />
     </section>
   );
 };
