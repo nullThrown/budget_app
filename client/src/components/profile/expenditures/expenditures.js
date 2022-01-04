867079bf7cf9c937a8c77dab3d5f9b5eeb5cbf3a
Weston Peters <westonrpr@yahoo.com>
2022-01-04 11:38:03 -0600

    select state from redux store

--- client/src/components/profile/expenditures/expenditures.js
+++ client/src/components/profile/expenditures/expenditures.js
@@ -2,14 +2,17 @@ import React, { useState } from 'react';
 import '../expenditures/expenditures.css';
 import { Header } from './header';
 import { Table } from './table';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Expenditures = ({ data }) => {
+  const selectExpenditures = (state) => state.expenditures;
+  const expenditures = useSelector(selectExpenditures);
   return (
     <div className='card expenditures-box'>
       <section className='exp-box'>
         <Header />
         <main className='main'>
-          <Table data={data} />
+          <Table data={expenditures} />
         </main>
       </section>
     </div>
