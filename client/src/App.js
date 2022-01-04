285a0c3f70275d2c9d3252a9ce485302b520439b
Weston Peters <westonrpr@yahoo.com>
2022-01-04 11:24:04 -0600

    add routes for new pages

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 //import from single page and/or use lazy loading
 import Login from './pages/login';
 import Signup from './pages/signup';
+import SignupSuccess from './pages/signupSuccess';
+import CreateProfile from './pages/createProfile';
 import Account from './pages/account';
 import Home from './pages/home';
 import Landing from './pages/landing';
@@ -13,13 +15,14 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/account' element={<Account />} />
-        <Route path='/home' element={<Home />} />
         <Route path='/' element={<Landing />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/signup-successful' element={<SignupSuccess />} />
+        <Route path='/create-profile' element={<CreateProfile />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/home' element={<Home />} />
         <Route path='/year' element={<Year />} />
-
+        <Route path='/account' element={<Account />} />
         {/* error path */}
         <Route path='/not-found' element={<NotFound />} />
       </Routes>
