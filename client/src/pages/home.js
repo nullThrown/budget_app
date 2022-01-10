9fdb1ba1c499ab437c9e61631fad498ccdfb5898
Weston Peters <westonrpr@yahoo.com>
2022-01-10 15:33:44 -0600

    add dispatch actions to get data from API

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { getProfile } from '../features/profile/profileSlice';
+import { getExpByCurrentMonth } from '../features/expenditures/expendituresSlice';
 import Overview from '../components/profile/overview/overview';
 import Expenditures from '../components/profile/expenditures/expenditures';
 import Header from '../components/profile/header/header';
@@ -7,14 +10,19 @@ import Banner from '../components/profile/banner/banner';
 import Nav from '../components/nav/nav';
 import Recurring from '../components/profile/recurring/recurring';
 import { categories, barData, doughnutData } from '../data/currentMonth';
+
 const Home = () => {
+  const dispatch = useDispatch();
+
+  dispatch(getProfile());
+  dispatch(getExpByCurrentMonth());
   return (
     <div>
       <Nav />
       <main className='main-container'>
         <Header />
         <Banner />
-        <Overview doughnut={doughnutData} />
+        <Overview doughnutData={doughnutData} />
         <Recurring />
         {/* import barData as means of placing graph config outside of component file */}
         <BudgetGraph data={barData} categories={categories} />
