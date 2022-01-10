37e53a480e9c4319cdd25cf73d5c91b12b1f42f4
Weston Peters <westonrpr@yahoo.com>
2022-01-10 15:24:55 -0600

    remove getProfile redux dispatch action

--- client/src/components/profile/budget/budgetGraph.js
+++ client/src/components/profile/budget/budgetGraph.js
@@ -1,21 +1,15 @@
-import { useEffect, useState } from 'react';
 import { Bar } from 'react-chartjs-2';
 import './budgetGraph.css';
 import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
-import { getProfile } from '../../../features/profile/profileSlice';
+
 const BudgetGraph = ({ data, categories }) => {
-  const dispatch = useDispatch();
   const selectPayments = (state) => {
     return state.profile.recurringPayments.map(
       ({ total, budget, category }) => ({ total, budget, category })
     );
   };
-  dispatch(getProfile());
   const payments = useSelector(selectPayments);
-  console.log(payments);
   const expenditures = useSelector((state) => state.expenditures);
-  const [budgetData, setBudgetData] = useState([]);
 
   return (
     <section className='card monthly-specifics'>
