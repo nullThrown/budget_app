91fd24527a80d8f474a3656f79c529835fa0cf4a
Weston Peters <westonrpr@yahoo.com>
2022-01-15 11:13:40 -0600

    convert all errors into error type variables

--- middleware/validation/validate.js
+++ middleware/validation/validate.js
@@ -1,9 +1,12 @@
 const { validationResult } = require('express-validator');
+const { invalid_data } = require('../../util/errorTypes');
 
 const validate = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+    return res
+      .status(422)
+      .json({ error: invalid_data, errors: errors.array() });
   }
   next();
 };
