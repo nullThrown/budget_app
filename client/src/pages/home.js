089b49ae03e7e58b3eea101cfd1c6a6a2a6c7a67
Weston Peters <westonrpr@yahoo.com>
2022-01-15 12:25:36 -0600

    add Loading component

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getProfile } from '../features/profile/profileSlice';
 import { getExpByCurrentMonth } from '../features/expenditures/expendituresSlice';
+import Loading from '../components/common/loading/loading';
 import Overview from '../components/profile/overview/overview';
 import Expenditures from '../components/profile/expenditures/expenditures';
 import Header from '../components/profile/header/header';
@@ -22,8 +23,8 @@ const Home = () => {
 
   if (loadingStatus === 'loading') {
     return (
-      <main className='main-container'>
-        <p>Loading...</p>
+      <main className='center-container'>
+        <Loading />;
       </main>
     );
   }
