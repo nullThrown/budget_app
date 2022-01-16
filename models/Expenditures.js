3f1a93568b7973de3dd40579664bc5a8ad03da1b
Weston Peters <westonrpr@yahoo.com>
2022-01-16 12:12:58 -0600

    add 'description' field

--- models/Expenditures.js
+++ models/Expenditures.js
@@ -1,19 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const expendituresSchema = new Schema([
-  {
-    user: { type: Schema.Types.ObjectId, ref: 'user' },
-    expenses: [
-      {
-        title: { type: String, maxlength: 30, required: true },
-        amount: { type: Number, required: true },
-        necessity: { type: Boolean, default: false, required: true },
-        category: { type: String, maxlength: 30, required: true },
-        date: { type: Date, default: Date.now() },
-      },
-    ],
-  },
-]);
+const expendituresSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: 'user' },
+  expenses: [
+    {
+      title: { type: String, maxlength: 30, required: true },
+      amount: { type: Number, required: true },
+      description: String,
+      necessity: { type: Boolean, default: false, required: true },
+      category: { type: String, maxlength: 30, required: true },
+      date: { type: Date, default: Date.now() },
+    },
+  ],
+});
 
 module.exports = mongoose.model('expenditures', expendituresSchema);
