97fb61d591be0e4f994afba58eebc8550ca0d023
Weston Peters <westonrpr@yahoo.com>
2022-01-16 11:31:35 -0600

    change selectors for new redux model & add createExpense dispatch for testing

--- client/src/components/profile/expenditures/expenditures.js
+++ client/src/components/profile/expenditures/expenditures.js
@@ -3,12 +3,34 @@ import '../expenditures/expenditures.css';
 import { Header } from './header';
 import { Table } from './table';
 import { useSelector, useDispatch } from 'react-redux';
+import { createExpense } from '../../../features/finance/financeReducer';
+
+const dummyExpense = {
+  title: 'length-1 test',
+  amount: 11.11,
+  description: 'testing grabbing last element',
+  necessity: true,
+  category: 'testing',
+};
 
 const Expenditures = ({ data }) => {
-  const selectExpenditures = (state) => state.expenditures;
+  const dispatch = useDispatch();
+  const selectExpenditures = (state) => state.finance.expenses;
   const expenditures = useSelector(selectExpenditures);
+
+  const handleNewExpense = (e) => {
+    e.preventDefault();
+    dispatch(createExpense(dummyExpense));
+  };
+
   return (
     <div className='card expenditures-box'>
+      <button
+        className='btn btn-submit'
+        type='submit'
+        onClick={handleNewExpense}>
+        Add Expense
+      </button>
       <section className='exp-box'>
         <Header />
         <main className='main'>
