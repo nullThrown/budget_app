e013b826ad86fe31e2a7e19b541d84a92530c1fb
Weston Peters <westonrpr@yahoo.com>
2022-01-17 17:29:04 -0600

    add functionality for choosing month/year data report

--- client/src/components/profile/banner/banner.js
+++ client/src/components/profile/banner/banner.js
@@ -1,22 +1,86 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './banner.css';
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
-import {IoMdAddCircleOutline} from 'react-icons/io'
+import { IoMdAddCircleOutline } from 'react-icons/io';
+import months from '../../../data/months';
+import { useDispatch } from 'react-redux';
+import { getFinancialData } from '../../../features/finance/financeReducer';
+
+const date = new Date();
+const currentMonthNum = date.getMonth();
+const currentYear = date.getFullYear();
+console.log('these values are being reassigned');
 const Banner = () => {
+  const dispatch = useDispatch();
+
+  const [monthStr, setMonthStr] = useState(null);
+  const [monthNum, setMonthNum] = useState(currentMonthNum);
+  const [year, setYear] = useState(currentYear);
+  const [isCurrentDate, setIsCurrentDate] = useState(true);
+
+  const onPrevClick = () => {
+    if (monthNum === 0) {
+      setMonthNum(11);
+      setYear(year - 1);
+    } else {
+      setMonthNum(monthNum - 1);
+    }
+    // dispatch(getFinancialData(year, monthNum));
+  };
+
+  const onNextClick = () => {
+    if (monthNum === currentMonthNum && year === currentYear) {
+      return;
+    } else if (monthNum === 11) {
+      setMonthNum(0);
+      setYear(year + 1);
+    } else {
+      setMonthNum(monthNum + 1);
+    }
+    dispatch(getFinancialData(year, monthNum));
+  };
+
+  useEffect(() => {
+    setMonthStr(months[monthNum]);
+  }, [monthNum]);
+
+  useEffect(() => {
+    if (monthNum === currentMonthNum && year === currentYear) {
+      setIsCurrentDate(true);
+    } else {
+      setIsCurrentDate(false);
+    }
+  }, [monthNum, year]);
+
+  useEffect(() => {
+    // dispatch(getFinancialData(year, monthNum));
+  }, [monthNum]);
   return (
     <div className='banner'>
       <div className='banner-box'>
-        <button className='btn arrow arrow-right'>
-          <AiOutlineArrowLeft className='' />
+        <button
+          type='button'
+          className='btn arrow arrow-left'
+          onClick={onPrevClick}>
+          <AiOutlineArrowLeft />
         </button>
-        <span className='heading-4 banner-date'>January 2021</span>
+        <span className='heading-4 banner-date'>
+          {monthStr} {year}
+        </span>
 
-        <button className='btn arrow arrow-left'>
+        <button
+          type='button'
+          className='btn arrow arrow-right'
+          onClick={onNextClick}
+          disabled={isCurrentDate}>
           <AiOutlineArrowRight />
         </button>
       </div>
       {/* <button type='button' className='btn banner__add-new'>Add New<GrAddCircle className='add-new-btn'/></button> */}
-      <button type='button' className='btn banner__add-new'>Add New<IoMdAddCircleOutline className='add-new-btn'/></button>
+      <button type='button' className='btn banner__add-new'>
+        Add New
+        <IoMdAddCircleOutline className='add-new-btn' />
+      </button>
     </div>
   );
 };
