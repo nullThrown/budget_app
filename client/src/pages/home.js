dc8f105cd4e12e5175517932f5c38950f58c30fd
Weston Peters <westonrpr@yahoo.com>
2022-01-17 22:30:07 -0600

    remove dispatch to get financial data

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,54 +1,37 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import Loading from '../components/common/loading/loading';
 import ServerError from '../components/alert/serverError';
 import Overview from '../components/profile/overview/overview';
 import Expenditures from '../components/profile/expenditures/expenditures';
-import Header from '../components/profile/header/header';
 import BudgetGraph from '../components/profile/budget/budgetGraph';
-import Banner from '../components/profile/banner/banner';
-import Nav from '../components/nav/nav';
 import Recurring from '../components/profile/recurring/recurring';
 import { categories, barData, doughnutData } from '../data/currentMonth';
-import { getFinancialData } from '../features/finance/financeReducer';
 
 const Home = () => {
-  const dispatch = useDispatch();
-
   const loadingStatus = useSelector((state) => state.finance.status);
 
-  useEffect(() => {
-    const date = new Date();
-    dispatch(getFinancialData(date.getFullYear(), date.getMonth()));
-  }, []);
-
-  // if (loadingStatus === 'loading') {
-  //   return (
-  //     <main className='center-container'>
-  //       <Loading />
-  //     </main>
-  //   );
-  // }
-  // if (loadingStatus === 'failed') {
-  //   return (
-  //     <main className='center-container'>
-  //       <ServerError />
-  //     </main>
-  //   );
-  // }
-  return (
-    <div>
-      <Nav />
-      <main className='main-container'>
-        <Header />
-        <Banner />
-        <Overview doughnutData={doughnutData} />
-        <Recurring />
-        {/* import barData as means of placing graph config outside of component file */}
-        <BudgetGraph data={barData} categories={categories} />
-        <Expenditures />
+  if (loadingStatus === 'loading') {
+    return (
+      <main className='center-container'>
+        <Loading />
       </main>
-    </div>
+    );
+  }
+  if (loadingStatus === 'failed') {
+    return (
+      <main className='center-container'>
+        <ServerError />
+      </main>
+    );
+  }
+  return (
+    <>
+      <Overview doughnutData={doughnutData} />
+      <Recurring />
+      <BudgetGraph data={barData} categories={categories} />
+      <Expenditures />
+    </>
   );
 };
 
