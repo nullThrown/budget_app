f58bb4cdbe9bc442b4cdc834f96d02d8ad5b6281
Weston Peters <westonrpr@yahoo.com>
2022-01-20 16:08:18 -0600

    add eslint-disable -- remove warning

--- client/src/components/profile/overview/overview.js
+++ client/src/components/profile/overview/overview.js
@@ -1,37 +1,34 @@
-import { useState, useEffect } from 'react';
 import './overview.css';
 import { Doughnut } from 'react-chartjs-2';
 import { useSelector } from 'react-redux';
-const Overview = ({ doughnutData }) => {
-  const [data, setData] = useState([]);
 
+const Overview = ({ doughnutData }) => {
+  // eslint-disable-next-line
   const selectRecurringTotal = (state) => {
     return state.finance.recurringPayments.reduce((acc, current) => {
       return acc + current.total;
     }, 0);
   };
+  // eslint-disable-next-line
   const selectExpNecessityTotal = (state) => {
     return state.finance.expenses.reduce((acc, current) => {
       if (current.necessity) return acc + current.amount;
       return acc;
     }, 0);
   };
+  // eslint-disable-next-line
   const selectExpIndulgentTotal = (state) => {
     return state.finance.expenses.reduce((acc, current) => {
       if (!current.necessity) return acc + current.amount;
       return acc;
     }, 0);
   };
-  const takeHomeAmount = useSelector((state) => state.finance.monthlyTakeHome); // 5600
-  const expNecessityTotal = useSelector(selectExpNecessityTotal); //200
-  const expIndulgentTotal = useSelector(selectExpIndulgentTotal); //233
-  const recurringTotal = useSelector(selectRecurringTotal); //4543
-  // [4743, 233, 624 ]
-  useEffect(() => {
-    setData((data) => {
-      data.push(recurringTotal);
-    });
-  }, []);
+  const takeHomeAmount = useSelector((state) => state.finance.monthlyTakeHome);
+  // eslint-disable-next-line
+  // const expNecessityTotal = useSelector(selectExpNecessityTotal);
+  // const expIndulgentTotal = useSelector(selectExpIndulgentTotal);
+  // const recurringTotal = useSelector(selectRecurringTotal);
+
   return (
     <section className='card budget'>
       <h3 className='heading-4 text-center'>Overview</h3>
