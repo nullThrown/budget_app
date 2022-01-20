7e49e728fffa23093b6028d5be479017f120f49c
Weston Peters <westonrpr@yahoo.com>
2022-01-20 16:34:15 -0600

    initial commit

---
+++ util/responseTypes.js
@@ -0,0 +1,22 @@
+// error
+const unauthenticated = 'unauthenticated';
+const invalid_data = 'invalid_data';
+const invalid_credentials = 'invalid_credentials';
+const server_error = 'server_error';
+const email_already_exists = 'email_already_exists';
+const resource_not_found = 'resource_not_found';
+// success
+const resource_created = 'resource_created';
+const resource_deleted = 'resource_deleted';
+const resource_edited = 'resource_edited';
+
+module.exports = {
+  unauthenticated,
+  invalid_data,
+  invalid_credentials,
+  server_error,
+  email_already_exists,
+  resource_created,
+  resource_deleted,
+  resource_edited,
+};
