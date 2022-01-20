5575332ae7ef155d0cfffea436870b3bb10a1a9e
Weston Peters <westonrpr@yahoo.com>
2022-01-20 16:03:39 -0600

    comment out code to remove es lint warnings

--- client/src/components/profile/budget/budgetGraph.js
+++ client/src/components/profile/budget/budgetGraph.js
@@ -1,16 +1,15 @@
 import { Bar } from 'react-chartjs-2';
 import './budgetGraph.css';
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 
 const BudgetGraph = ({ data, categories }) => {
-  const selectPayments = (state) => {
-    return state.finance.recurringPayments.map(
-      ({ total, budget, category }) => ({ total, budget, category })
-    );
-  };
-  const payments = useSelector(selectPayments);
-  const expenditures = useSelector((state) => state.finance.expenses);
-
+  // const selectPayments = (state) => {
+  //   return state.finance.recurringPayments.map(
+  //     ({ total, budget, category }) => ({ total, budget, category })
+  //   );
+  // };
+  // const payments = useSelector(selectPayments);
+  // const expenditures = useSelector((state) => state.finance.expenses);
   return (
     <section className='card monthly-specifics'>
       <h2 className='heading-4 text-center'>Budget</h2>
