4dc4564b68169a246403854cc166ab0e692c73c2
Weston Peters <westonrpr@yahoo.com>
2022-01-22 15:21:25 -0600

    import and add functionality to add new modal

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Nav from '../components/nav/nav';
 import Header from '../components/profile/header/header';
 import Banner from '../components/profile/banner/banner';
+import PaymentModal from '../components/common/modal/paymentModal';
+
 const Profile = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
   return (
     <div>
+      <PaymentModal isOpen={isOpen} closeModal={closeModal} />
       <Nav />
       <main className='main-container'>
         <Header />
-        <Banner />
+        <Banner openModal={openModal} />
         <Outlet />
       </main>
     </div>
