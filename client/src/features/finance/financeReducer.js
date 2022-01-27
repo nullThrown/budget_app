f98549838fb59599831ebde74e518d57db649f70
Weston Peters <westonrpr@yahoo.com>
2022-01-27 16:55:23 -0600

    add state changes for recurring array & moved expense middleware to separate folder

--- client/src/features/finance/financeReducer.js
+++ client/src/features/finance/financeReducer.js
@@ -47,12 +47,32 @@ export default function financeReducer(state = initialState, action) {
           ...state.expenses.filter((expense) => expense._id !== payload),
         ],
       };
-    case 'finance/addRecurring':
-      return;
+    case 'finance/createRecurring':
+      return {
+        ...state,
+        recurringPayments: payload,
+      };
     case 'finance/editRecurring':
-      return;
+      return { ...state, recurringPayments: payload };
     case 'finance/deleteRecurring':
-      return;
+      const { id, category } = payload;
+      return {
+        ...state,
+        recurringPayments: [
+          ...state.recurringPayments.map((recurring) => {
+            if (recurring.category === category) {
+              return {
+                ...recurring,
+                payments: [
+                  ...recurring.payments.filter((pay) => pay._id !== id),
+                ],
+              };
+            } else {
+              return recurring;
+            }
+          }),
+        ],
+      };
 
     default:
       return state;
@@ -86,47 +106,6 @@ export const getFinancialData = (year, month) => async (dispatch) => {
       // token is not valid... requires a redirect to login
     } else if (status >= 500 && data.error === 'server_error') {
       dispatch({ type: 'finance/dataLoadError' });
-    }
-  }
-};
-
-export const createExpense = (expense) => async (dispatch) => {
-  try {
-    const newExpense = await axios.post(
-      'http://localhost:4000/api/exp/create-new',
-      expense,
-      { withCredentials: true }
-    );
-    console.log(newExpense.data);
-    dispatch({ type: 'finance/createExpense', payload: newExpense.data });
-  } catch (err) {
-    const { data, status } = err.response;
-    if (status >= 400 && status < 500 && data.error === 'unauthenticated') {
-      // token is not valid... requires a redirect to login
-    } else if (status >= 500 && data.error === 'server_error') {
-      // dispatch some type of server error
-    }
-  }
-};
-// remove var deleteExpense... just run as a function
-export const deleteExpense = (expenseId) => async (dispatch) => {
-  try {
-    const deletedExpense = await axios.delete(
-      `http://localhost:4000/api/exp/delete/${expenseId}`,
-      { withCredentials: true }
-    );
-    dispatch({ type: 'finance/deleteExpense', payload: expenseId });
-  } catch (err) {
-    const { data, status } = err.response;
-    if (status >= 400 && status < 500) {
-      if (data.error === 'unauthenticated') {
-        // token is not valid... requires a redirect to login
-      }
-      if (data.error === 'resource_not_found') {
-        console.log(data.error);
-      }
-    } else if (status >= 500 && data.error === 'server_error') {
-      // dispatch some type of server error
     }
   }
 };
