1c2c3058c42fc0f8c60f46195330801af640b368
Weston Peters <westonrpr@yahoo.com>
2022-01-30 16:40:51 -0600

    remove all testing aspects
    Crude tests were done for CRUD expense dispatch actions

--- client/src/components/profile/expenditures/expenditures.js
+++ client/src/components/profile/expenditures/expenditures.js
@@ -1,49 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../expenditures/expenditures.css';
 import { Header } from './header';
 import { Table } from './table';
-import { useSelector, useDispatch } from 'react-redux';
-import {
-  createExpense,
-  deleteExpense,
-} from '../../../features/expenses/middleware';
+import { useSelector } from 'react-redux';
 
-const dummyExpense = {
-  title: 'item 3',
-  amount: 11.11,
-  description: 'test item',
-  necessity: true,
-  category: 'testing',
-};
+const Expenditures = ({ openModal }) => {
+  const expenses = useSelector((state) => state.expenses.data);
 
-const Expenditures = ({ data }) => {
-  const dispatch = useDispatch();
-  const selectExpenses = (state) => state.expenses.data;
-  const expenditures = useSelector(selectExpenses);
-
-  const handleNewExpense = (e) => {
-    e.preventDefault();
-    dispatch(createExpense(dummyExpense));
-  };
-  const handleDeleteExpense = (e) => {
-    e.preventDefault();
-    dispatch(deleteExpense('61e9f361103bf5119c38dc2a'));
-  };
   return (
-    <div className='card expenditures-box'>
-      <button
-        className='btn btn-submit'
-        type='submit'
-        onClick={handleDeleteExpense}>
-        Del Expense
-      </button>
-      <section className='exp-box'>
-        <Header />
-        <main className='main'>
-          <Table data={expenditures} />
-        </main>
-      </section>
-    </div>
+    <>
+      <div className='card expenditures-box'>
+        <section className='exp-box'>
+          <Header />
+          <main className='main'>
+            <Table expenses={expenses} openModal={openModal} />
+          </main>
+        </section>
+      </div>
+    </>
   );
 };
 
