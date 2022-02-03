5044a151e355f3e87b1c965af1accb1c4ac5fbef
Weston Peters <westonrpr@yahoo.com>
2022-02-03 11:08:54 -0600

    move table cell to own component

--- client/src/components/profile/expenditures/table.js
+++ client/src/components/profile/expenditures/table.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-
+import TableCell from './tableCell';
 export const Table = ({ expenses, openModal }) => {
   return (
     <table>
@@ -20,32 +20,7 @@ export const Table = ({ expenses, openModal }) => {
           const readableDate = newDate.toLocaleDateString('en-US');
 
           return (
-            <>
-              <tr key={exp._id} className='table-row-data'>
-                <td className='table-cell-date'>{exp.title}</td>
-                <td className='table-cell-date'>{readableDate}</td>
-                <td className='table-cell-desc'>{exp.description}</td>
-                <td className='table-cell-category'>{exp.category}</td>
-                <td className='table-cell-necessity'>
-                  <span
-                    className={
-                      'text-sm ' +
-                      (exp.necessity ? 'text-bg-green' : 'text-bg-red')
-                    }>
-                    {exp.necessity ? ' necessity' : ' indulgent'}
-                  </span>
-                </td>
-
-                <td className='table-cell-amount'>${exp.amount}</td>
-                <td className='table-cell-edit'>
-                  <button
-                    className='btn link'
-                    onClick={() => openModal(exp._id)}>
-                    edit
-                  </button>
-                </td>
-              </tr>
-            </>
+            <TableCell exp={exp} date={readableDate} openModal={openModal} />
           );
         })}
       </tbody>
