65763ac754c866c89e7969854ba4504b274cd3c5
Weston Peters <westonrpr@yahoo.com>
2022-02-03 14:21:23 -0600

    add edit dispatch & add status conditional renders

--- client/src/components/common/modal/expense/modal.js
+++ client/src/components/common/modal/expense/modal.js
@@ -5,6 +5,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import Loading from '../../loading/loading';
 import Success from '../success';
 import customStyles from '../styles';
+import { editExpense } from '../../../../features/expenses/middleware';
 
 const ExpenseModal = ({ isOpen, closeModal, expenseId }) => {
   const dispatch = useDispatch();
@@ -26,29 +27,12 @@ const ExpenseModal = ({ isOpen, closeModal, expenseId }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // dispatch edit Expense
+    dispatch(editExpense(expense));
   };
 
   useEffect(() => {
     setExpense(expenseItem || {});
   }, [expenseItem]);
-
-  if (status === 'loading') {
-    return (
-      <div className='center-container'>
-        <Loading />
-      </div>
-    );
-  }
-  if (status === 'success') {
-    return (
-      <Success
-        text='Payment edited Successfully!'
-        actionCreator={{ type: 'expenses/returnToIdle' }}
-        closeModal={closeModal}
-      />
-    );
-  }
 
   const onRequestClose = () => {
     dispatch({ type: 'expenses/returnToIdle' });
@@ -67,58 +51,75 @@ const ExpenseModal = ({ isOpen, closeModal, expenseId }) => {
         onClick={closeModal}>
         X
       </button>
-      <section className='modal-expense-sect'>
-        <form className='modal__payment-form'>
-          <p className='heading-6 text-center modal__payment-title'>
-            Edit Expense
-          </p>
-          <input
-            type='text'
-            className='input-border-bottom input-expense__title'
-            name='title'
-            placeholder='title'
-            onChange={onInputChange}
-            value={expense.title}
-          />
-          <input
-            name='category'
-            list='payment-categories'
-            placeholder='category'
-            className='input-border-bottom input-expense__categories'
-            onChange={onInputChange}
-            value={expense.category}
-          />
-          <input
-            type='text'
-            className='input-border-bottom input-expense__desc'
-            name='description'
-            placeholder='description'
-            onChange={onInputChange}
-            value={expense.description}
-          />
-          <input
-            type='text'
-            className='input-border-bottom input-expense__amount'
-            name='amount'
-            placeholder='amount'
-            onChange={onInputChange}
-            value={expense.amount}
-          />
-          <datalist id='payment-categories'>
-            {categories.map((cat) => {
-              return <option value={cat}></option>;
-            })}
-          </datalist>
 
-          <button
-            type='submit'
-            className='btn btn-submit modal__expense-submit-btn'
-            onClick={handleSubmit}>
-            Submit
-          </button>
-          {status === 'error' && <p>Something went wrong :( try again</p>}
-        </form>
-      </section>
+      {status === 'loading' && (
+        <div className='center-container'>
+          <Loading />
+        </div>
+      )}
+
+      {status === 'edit_success' && (
+        <Success
+          text='Payment edited Successfully!'
+          actionCreator={{ type: 'expenses/returnToIdle' }}
+          closeModal={closeModal}
+        />
+      )}
+
+      {(status === 'idle' || status === 'error') && (
+        <section className='modal-expense-sect'>
+          <form className='modal__payment-form'>
+            <p className='heading-6 text-center modal__payment-title'>
+              Edit Expense
+            </p>
+            <input
+              type='text'
+              className='input-border-bottom input-expense__title'
+              name='title'
+              placeholder='title'
+              onChange={onInputChange}
+              value={expense.title}
+            />
+            <input
+              name='category'
+              list='payment-categories'
+              placeholder='category'
+              className='input-border-bottom input-expense__categories'
+              onChange={onInputChange}
+              value={expense.category}
+            />
+            <input
+              type='text'
+              className='input-border-bottom input-expense__desc'
+              name='description'
+              placeholder='description'
+              onChange={onInputChange}
+              value={expense.description}
+            />
+            <input
+              type='text'
+              className='input-border-bottom input-expense__amount'
+              name='amount'
+              placeholder='amount'
+              onChange={onInputChange}
+              value={expense.amount}
+            />
+            <datalist id='payment-categories'>
+              {categories.map((cat) => {
+                return <option value={cat}></option>;
+              })}
+            </datalist>
+
+            <button
+              type='submit'
+              className='btn btn-submit modal__expense-submit-btn'
+              onClick={handleSubmit}>
+              Submit
+            </button>
+            {status === 'error' && <p>Something went wrong :( try again</p>}
+          </form>
+        </section>
+      )}
     </Modal>
   );
 };
