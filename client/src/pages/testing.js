d2604649f96f40806282eafdfbe0a90868e1da04
Weston Peters <westonrpr@yahoo.com>
2022-02-05 12:47:37 -0600

    import Alert component for visual inspection

--- client/src/pages/testing.js
+++ client/src/pages/testing.js
@@ -1,6 +1,10 @@
 import React from 'react';
-
+import { Alert } from '../components/alert/alert';
 const Testing = () => {
-  return <main className='center-container'></main>;
+  return (
+    <main className='center-container'>
+      <Alert msg={'something went wrong'} />
+    </main>
+  );
 };
 export default Testing;
