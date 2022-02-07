e8a7ba904c869924f0ae7802b2f854907235bae4
Weston Peters <westonrpr@yahoo.com>
2022-02-07 12:46:06 -0600

    add dispatch for updateCategories
    *upon createRecurring, a dispatch is sent to update state for categories array on profile state prop *rename all axios fetch calls binding variable to 'res'

--- client/src/features/recurring/middleware.js
+++ client/src/features/recurring/middleware.js
@@ -1,17 +1,25 @@
 import axios from 'axios';
 import handleErrors from '../../util/api/errorHandler';
+
 export const createRecurring = (recurring) => async (dispatch) => {
   dispatch({ type: 'recurring/dataLoading' });
   try {
-    const newRecurring = await axios.post(
+    const res = await axios.post(
       'http://localhost:4000/api/profile/recurring/create',
       recurring,
       {
         withCredentials: true,
       }
     );
-    console.log(newRecurring.data);
-    dispatch({ type: 'recurring/createRecurring', payload: newRecurring.data });
+    const { data } = res;
+    dispatch({
+      type: 'recurring/createRecurring',
+      payload: data.recurringPayments,
+    });
+    dispatch({
+      type: 'profile/updateCategories',
+      payload: data.categories,
+    });
   } catch (err) {
     handleErrors(err, 'recurring', dispatch);
   }
@@ -19,15 +27,14 @@ export const createRecurring = (recurring) => async (dispatch) => {
 
 export const editRecurring = (recurring) => async (dispatch) => {
   try {
-    const newRecurring = await axios.put(
+    const res = await axios.put(
       'http://localhost:4000/api/profile/recurring/edit',
       recurring,
       {
         withCredentials: true,
       }
     );
-    console.log(newRecurring.data);
-    dispatch({ type: 'finance/editRecurring', payload: newRecurring.data });
+    dispatch({ type: 'finance/editRecurring', payload: res.data });
   } catch (err) {
     handleErrors(err, 'recurring', dispatch);
   }
@@ -37,13 +44,12 @@ export const deleteRecurring =
   ({ id, category }) =>
   async (dispatch) => {
     try {
-      const newRecurring = await axios.delete(
+      const res = await axios.delete(
         `http://localhost:4000/api/profile/recurring/delete/${id}/${category}`,
         {
           withCredentials: true,
         }
       );
-      console.log('axios delete success');
       dispatch({ type: 'finance/deleteRecurring', payload: { id, category } });
     } catch (err) {
       handleErrors(err, 'recurring', dispatch);
