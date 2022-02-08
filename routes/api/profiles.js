03ac2ecbccdc41e49c15955235cb774742b76ec5
Weston Peters <westonrpr@yahoo.com>
2022-02-08 11:09:46 -0600

    whitespace changes... I think
    don't quote me on that

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -121,184 +121,190 @@ router.delete(
   }
 );
 
-// ROUTE    POST api/profile/
+// ROUTE    POST api/profile/create
 // DESC     Create or update new profile
 // ACCESS   Private
-router.post('/', verifyToken, validateProfile(), validate, async (req, res) => {
-  const {
-    paycheckAmount,
-    salarySchedule,
-    housingPayment,
-    housingInsurance,
-    vehicleLoan,
-    vehicleInsurance,
-    cellPlan,
-    cellLoan,
-    internetPlan,
-    childcareTuition,
-    childcareDaycare,
-    healthInsurance,
-    healthFitness,
-    debtStudent,
-    debtCredit,
-    retirementIra,
-    retirementBrokerage,
-    retirement401k,
-  } = req.body;
+router.post(
+  '/create',
+  verifyToken,
+  validateProfile(),
+  validate,
+  async (req, res) => {
+    const {
+      paycheckAmount,
+      salarySchedule,
+      housingPayment,
+      housingInsurance,
+      vehicleLoan,
+      vehicleInsurance,
+      cellPlan,
+      cellLoan,
+      internetPlan,
+      childcareTuition,
+      childcareDaycare,
+      healthInsurance,
+      healthFitness,
+      debtStudent,
+      debtCredit,
+      retirementIra,
+      retirementBrokerage,
+      retirement401k,
+    } = req.body;
 
-  const newProfile = {
-    user: req.user.id,
-    paycheckAmount,
-    salarySchedule,
-    recurringPayments: [
-      {
-        // housing expenses
-        total: Number(housingPayment) + Number(housingInsurance),
-        category: 'housing',
-        payments: [
-          {
-            amount: housingPayment,
-            name: 'mortgage/rent',
-          },
-          {
-            amount: housingInsurance,
-            name: 'insurance',
-          },
-        ],
-      },
-      {
-        // vehicle expenses
-        total: Number(vehicleLoan) + Number(vehicleInsurance),
-        category: 'vehicle',
-        payments: [
-          {
-            amount: vehicleLoan,
-            name: 'loan',
-          },
-          {
-            amount: vehicleInsurance,
-            name: 'insurance',
-          },
-        ],
-      },
-      {
-        // utility expenses
-        total: Number(cellPlan) + Number(cellLoan) + Number(internetPlan),
-        category: 'utilities',
-        payments: [
-          {
-            amount: cellPlan,
-            name: 'cell plan',
-          },
-          {
-            amount: cellLoan,
-            name: 'cell loan',
-          },
-          {
-            amount: internetPlan,
-            name: 'internet',
-          },
-        ],
-      },
-      {
-        // childcare expenses
-        total: Number(childcareTuition) + Number(childcareDaycare),
-        category: 'childcare',
-        payments: [
-          {
-            amount: childcareTuition,
-            name: 'tuition',
-          },
-          {
-            amount: childcareDaycare,
-            name: 'daycare',
-          },
-        ],
-      },
-      {
-        // health expenses
-        total: Number(healthInsurance) + Number(healthFitness),
-        category: 'health',
-        payments: [
-          {
-            amount: healthInsurance,
-            name: 'insurance',
-          },
-          {
-            amount: healthFitness,
-            name: 'fitness',
-          },
-        ],
-      },
-      {
-        // debt payment expenses
-        total: Number(debtStudent) + Number(debtCredit),
-        category: 'debt',
-        payments: [
-          {
-            amount: debtStudent,
-            name: 'student',
-          },
-          {
-            amount: debtCredit,
-            name: 'credit',
-          },
-        ],
-      },
-      {
-        // retirement expenses
-        total:
-          Number(retirement401k) +
-          Number(retirementIra) +
-          Number(retirementBrokerage),
-        category: 'retirement',
-        payments: [
-          {
-            amount: retirement401k,
-            name: '401k',
-          },
-          {
-            amount: retirementIra,
-            name: 'ira',
-          },
-          {
-            amount: retirementBrokerage,
-            name: 'brokerage',
-          },
-        ],
-      },
-    ],
-    categories: [
-      'housing',
-      'vehicle',
-      'utilites',
-      'childcare',
-      'health',
-      'debt',
-      'retirement',
-    ],
-  };
-
-  try {
-    let profile = await Profile.findOne({ user: req.user.id });
-    if (profile) {
-      profile = await Profile.findOneAndUpdate(
-        { user: req.user.id },
-        { ...newProfile, updatedAt: Date.now() },
+    const newProfile = {
+      user: req.user.id,
+      paycheckAmount,
+      salarySchedule,
+      recurringPayments: [
         {
-          returnOriginal: false,
-          useFindAndModify: false,
-        }
-      );
-    } else {
-      profile = new Profile(newProfile);
-      await profile.save();
+          // housing expenses
+          total: Number(housingPayment) + Number(housingInsurance),
+          category: 'housing',
+          payments: [
+            {
+              amount: housingPayment,
+              name: 'mortgage/rent',
+            },
+            {
+              amount: housingInsurance,
+              name: 'insurance',
+            },
+          ],
+        },
+        {
+          // vehicle expenses
+          total: Number(vehicleLoan) + Number(vehicleInsurance),
+          category: 'vehicle',
+          payments: [
+            {
+              amount: vehicleLoan,
+              name: 'loan',
+            },
+            {
+              amount: vehicleInsurance,
+              name: 'insurance',
+            },
+          ],
+        },
+        {
+          // utility expenses
+          total: Number(cellPlan) + Number(cellLoan) + Number(internetPlan),
+          category: 'utilities',
+          payments: [
+            {
+              amount: cellPlan,
+              name: 'cell plan',
+            },
+            {
+              amount: cellLoan,
+              name: 'cell loan',
+            },
+            {
+              amount: internetPlan,
+              name: 'internet',
+            },
+          ],
+        },
+        {
+          // childcare expenses
+          total: Number(childcareTuition) + Number(childcareDaycare),
+          category: 'childcare',
+          payments: [
+            {
+              amount: childcareTuition,
+              name: 'tuition',
+            },
+            {
+              amount: childcareDaycare,
+              name: 'daycare',
+            },
+          ],
+        },
+        {
+          // health expenses
+          total: Number(healthInsurance) + Number(healthFitness),
+          category: 'health',
+          payments: [
+            {
+              amount: healthInsurance,
+              name: 'insurance',
+            },
+            {
+              amount: healthFitness,
+              name: 'fitness',
+            },
+          ],
+        },
+        {
+          // debt payment expenses
+          total: Number(debtStudent) + Number(debtCredit),
+          category: 'debt',
+          payments: [
+            {
+              amount: debtStudent,
+              name: 'student',
+            },
+            {
+              amount: debtCredit,
+              name: 'credit',
+            },
+          ],
+        },
+        {
+          // retirement expenses
+          total:
+            Number(retirement401k) +
+            Number(retirementIra) +
+            Number(retirementBrokerage),
+          category: 'retirement',
+          payments: [
+            {
+              amount: retirement401k,
+              name: '401k',
+            },
+            {
+              amount: retirementIra,
+              name: 'ira',
+            },
+            {
+              amount: retirementBrokerage,
+              name: 'brokerage',
+            },
+          ],
+        },
+      ],
+      categories: [
+        'housing',
+        'vehicle',
+        'utilites',
+        'childcare',
+        'health',
+        'debt',
+        'retirement',
+      ],
+    };
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+      if (profile) {
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { ...newProfile, updatedAt: Date.now() },
+          {
+            returnOriginal: false,
+            useFindAndModify: false,
+          }
+        );
+      } else {
+        profile = new Profile(newProfile);
+        await profile.save();
+      }
+      res.json(profile);
+    } catch (err) {
+      console.error({ err: [err.message, err.stack] });
+      res.status(500).json({ error: server_error });
     }
-    res.json(profile);
-  } catch (err) {
-    console.error({ err: [err.message, err.stack] });
-    res.status(500).json({ error: server_error });
   }
-});
+);
 
 module.exports = router;
