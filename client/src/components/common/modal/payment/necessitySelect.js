59c1f45cb339682bc01255d07b53ccae7b406d58
Weston Peters <westonrpr@yahoo.com>
2022-02-10 12:33:27 -0600

    initial commit

---
+++ client/src/components/common/modal/payment/necessitySelect.js
@@ -0,0 +1,35 @@
+import './necessitySelect.css';
+
+const NecessitySelect = ({ necessity, onInputChange, className }) => {
+  return (
+    <div className={`flex select-container ${className ? className : ''}`}>
+      <button
+        type='button'
+        name='necessity'
+        className={`btn btns-default ${
+          necessity === true ? 'necessity-selected-btn' : ''
+        }`}
+        onClick={onInputChange}>
+        Necessity
+      </button>
+      <div className='widget-container'>
+        <div
+          className={`widget ${
+            necessity === true ? 'necessity-selected' : 'indulgent-selected'
+          } `}></div>
+      </div>
+
+      <button
+        type='button'
+        name='indulgent'
+        className={`btn btns-default ${
+          necessity === false ? 'indulgent-selected-btn' : ''
+        }`}
+        onClick={onInputChange}>
+        Indulgent
+      </button>
+    </div>
+  );
+};
+
+export default NecessitySelect;
