8812117cbc9b384d9ba81f5f67a5d9d146d6303a
Weston Peters <westonrpr@yahoo.com>
2022-02-12 11:30:00 -0600

    remove callback param
    *validator functions only return true/false instead of calling callback with boolean values

--- client/src/util/validation/validatePassword.js
+++ client/src/util/validation/validatePassword.js
@@ -1,17 +1,11 @@
-// callback parameter is to be argued with React setState fn
-
 //string is 8 chars long
-export const checkStrLength = (string, cb) =>
-  string.length >= 8 ? cb(true) : cb(false);
+export const checkStrLength = (string) => string.length >= 8;
 
-// string contains number char
-export const checkStrNum = (string, cb) =>
-  string.match('.*[0-9].*') ? cb(true) : cb(false);
+// string contains num char
+export const checkStrNum = (string) => string.match('.*[0-9].*');
 
 // string contains special char
-export const checkStrSym = (string, cb) =>
-  string.match('(?=.*?[#?!@$%^&*-])') ? cb(true) : cb(false);
+export const checkStrSym = (string) => string.match('(?=.*?[#?!@$%^&*-])');
 
 // str contains uppercase char
-export const checkStrUpper = (string, cb) =>
-  string.match('(?=.*?[A-Z])') ? cb(true) : cb(false);
+export const checkStrUpper = (string) => string.match('(?=.*?[A-Z])');
