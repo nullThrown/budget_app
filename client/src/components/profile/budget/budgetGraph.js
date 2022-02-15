c9732354d315c804efb214ed2a7f0709adb8de8d
Weston Peters <westonrpr@yahoo.com>
2022-02-15 14:24:11 -0600

    remove preliminary attempt at organizing budget graph data

--- client/src/components/profile/budget/budgetGraph.js
+++ client/src/components/profile/budget/budgetGraph.js
@@ -1,76 +1,11 @@
+import { useEffect, useState } from 'react';
 import { Bar } from 'react-chartjs-2';
 import './budgetGraph.css';
 import { useSelector } from 'react-redux';
 
-export const categories = [
-  {
-    _id: 'aslkdsjad39',
-    title: 'vehicle',
-    budget: 500,
-  },
-  {
-    _id: 'aslkd3230-9',
-    title: 'misc',
-    budget: 400,
-  },
-  {
-    _id: 'kasdja29',
-    title: 'Dining',
-    budget: 200,
-  },
-  {
-    _id: 'asdjaj2093',
-    title: 'utlities',
-    budget: 400,
-  },
-];
-
-// will require an array of objects
-//
-// object shape
-// {
-//   title: 'vehicle',
-//   budget: 500,
-//   spent: 255,
-//},
-// this array will be passed into the barData function
-
-// restructure Model for profile.categories
-// category: { name: 'housing', budget: 500}
-// remove budget from recurring model
-
-// restructure profile.recurring
-// remove budget/total -- virtual function
-// remove payments obj
-// place recurring on top level within data array
-// rec: {
-//_id: ObjectId,
-// category: String,
-// name: String,
-// amount: Number,
-// necessity: Boolean,
-
-//
-
-//}
-//
-
-// select state.expenses
-// select state.recurring
-// create barData state
-
-// useEffect: add expenses total to each category
-// loop thru categories
-// nested loop thru expenses
-// if cat.title === exp.category
-// add nec.amount to spent
-
-// useEffect: add recurring total to each category
-// loop thru categories
-// nested loop thru recurring
-// if rec.cat.title ===
-
 const BudgetGraph = ({ barData }) => {
+  const categories = useSelector((state) => state.profile.categories);
+
   return (
     <section className='card monthly-specifics'>
       <h2 className='heading-4 text-center'>Budget</h2>
