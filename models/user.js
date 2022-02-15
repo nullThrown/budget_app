74d743e733641e20f7358a524015d36743039db0
Weston Peters <westonrpr@yahoo.com>
2022-02-15 14:28:56 -0600

    import fieldTypes and add timestamp to config

--- models/user.js
+++ models/user.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const { requiredStr } = require('./fieldTypes');
 
-const UserSchema = new Schema({
-  firstName: { type: String, maxlength: 100, required: true },
-  lastName: { type: String, maxlength: 100, required: true },
-  email: { type: String, maxlength: 50, required: true },
-  password: { type: String, required: true },
-  profile: { type: mongoose.Schema.Types.ObjectId, ref: 'Profile' },
-  isAdmin: { type: Boolean, default: false },
-  createdAt: { type: Date, default: Date.now() },
-});
+const UserSchema = new Schema(
+  {
+    profile: { type: mongoose.Schema.Types.ObjectId, ref: 'Profile' },
+    firstName: { ...requiredStr, maxlength: 32 },
+    lastName: { ...requiredStr, maxlength: 32 },
+    email: { ...requiredStr, maxlength: 32 },
+    password: { ...requiredStr, maxlength: 32 },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('User', UserSchema);
