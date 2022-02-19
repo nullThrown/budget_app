163ebc8338fe0c35062b81580684507254ea77d0
Weston Peters <westonrpr@yahoo.com>
2022-02-19 13:40:04 -0600

    add pagination functionality & place PaymentItem component into separate file

--- client/src/components/profile/recurring/recurring.js
+++ client/src/components/profile/recurring/recurring.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './recurring.css';
 import { useSelector, useDispatch } from 'react-redux';
+import PaymentItem from './paymentItem';
 import {
   createRecurring,
   editRecurring,
@@ -9,36 +10,69 @@ import {
 
 const Recurring = () => {
   const dispatch = useDispatch();
-  const selectPayments = (state) => state.recurring.data;
-  const payments = useSelector(selectPayments);
+  const [numOfPages, setNumOfPages] = useState(null);
+  const [pagesArray, setPagesArray] = useState([]);
+  const [currentCategories, setCurrentCategories] = useState([]);
+  const [categoriesPerPage, setCategoriesPerPage] = useState(3);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const payments = useSelector((state) => state.recurring.data);
+  const numOfCategories = useSelector((state) => state.recurring.data.length);
+
+  const handlePageChange = (e) => {};
+  useEffect(() => {
+    console.log('component mounted');
+  }, [currentPage]);
+  useEffect(() => {
+    setNumOfPages(Math.ceil(numOfCategories / categoriesPerPage));
+  }, [numOfCategories]);
+
+  useEffect(() => {
+    const arr = [];
+    for (let i = 0; i < numOfPages; i++) {
+      arr.push(i);
+    }
+    setPagesArray(arr);
+  }, [numOfPages]);
+
+  useEffect(() => {
+    const endIndex = currentPage * categoriesPerPage;
+    const startIndex = endIndex - categoriesPerPage;
+    setCurrentCategories(() => {
+      return payments.slice(startIndex, endIndex);
+    });
+  }, [currentPage, payments]);
+  useEffect(() => {
+    console.log(currentCategories);
+  }, [currentCategories]);
 
   return (
     <section className='card recurring'>
       <h2 className='heading-4 text-center'>Recurring</h2>
-      {payments.map((cat, _id) => {
-        const { category, payments } = cat;
-        return (
-          <PaymentItem key={_id} category={category} payments={payments} />
-        );
-      })}
+      <div className='card-main-content-box'>
+        <div className='payment__box'>
+          {currentCategories.map((cat, _id) => {
+            const { category, payments } = cat;
+            return (
+              <PaymentItem key={_id} category={category} payments={payments} />
+            );
+          })}
+        </div>
+        <div className='payment__pages'>
+          {pagesArray.map((page, i) => {
+            return (
+              <button
+                type='button'
+                key={i}
+                className='btn link mr-1-2'
+                onClick={() => setCurrentPage(page + 1)}>
+                {page + 1}
+              </button>
+            );
+          })}
+        </div>
+      </div>
     </section>
-  );
-};
-
-const PaymentItem = ({ category, payments }) => {
-  return (
-    <div className='payment__category'>
-      <p className=' payment__title text-sm'>{category}</p>
-      {payments.map((expense) => {
-        const { amount, name, _id } = expense;
-        return (
-          <div key={_id} className='flex payment__item'>
-            <p className='payment__item-title text-sm'>{name}</p>
-            <span className='payment__item-amt   text-sm'>$ {amount}</span>
-          </div>
-        );
-      })}
-    </div>
   );
 };
 
