b4dd02c032895e4345c03a4f3c98f69744e8e7aa
Weston Peters <westonrpr@yahoo.com>
2022-02-19 13:49:27 -0600

    add toggle nav functionality

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import Nav from '../components/nav/nav';
 import Header from '../components/profile/header/header';
@@ -8,13 +8,26 @@ const Profile = () => {
   const [isOpen, setIsOpen] = useState(false);
   const closeModal = () => setIsOpen(false);
   const openModal = () => setIsOpen(true);
+  const [isNavOpen, setIsNavOpen] = useState(null);
+
+  const toggleNav = () => setIsNavOpen(!isNavOpen);
+
+  useEffect(() => {
+    // forces the nav to open at 1150px screen width
+    const mediaQuery = window.matchMedia('(min-width: 1150px)');
+    const listener = () =>
+      mediaQuery.matches ? setIsNavOpen(true) : setIsNavOpen(false);
+
+    mediaQuery.addEventListener('change', listener);
+    return () => mediaQuery.removeEventListener('change', listener);
+  }, []);
 
   return (
     <div>
       <AddPayment isOpen={isOpen} closeModal={closeModal} />
-      <Nav />
+      <Nav isNavOpen={isNavOpen} toggleNav={toggleNav} />
       <main className='main-container'>
-        <Header />
+        <Header toggleNav={toggleNav} />
         <Banner openModal={openModal} />
         <Outlet />
       </main>
