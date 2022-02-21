18b3f49da1be82a2d54abc3a2813621b470668d2
Weston Peters <westonrpr@yahoo.com>
2022-02-20 21:00:14 -0600

    remove unused import

--- client/src/components/form/signup/signupForm.js
+++ client/src/components/form/signup/signupForm.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { PasswordCheck } from './passwordCheck';
 import { useDispatch, useSelector } from 'react-redux';
